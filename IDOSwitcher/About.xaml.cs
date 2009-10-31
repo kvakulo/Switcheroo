c286ff01a1e0247663eca8c4c510f8724ecd404d
James Sulak <jsulak@gmail.com>
2009-10-30 20:57:37 -0500

    Added switcheroo project (oops from rename).  Added installer.  Incomplete stab at about box.

---
+++ IDOSwitcher/About.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Switcheroo
+{
+    /// <summary>
+    /// Interaction logic for About.xaml
+    /// </summary>
+    public partial class About : Window
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+    }
+}
