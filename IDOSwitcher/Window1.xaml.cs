cd95ffbdd7a3958e7f30e23943d8bcb730451d44
James Sulak <jsulak@gmail.com>
2009-11-01 13:48:10 -0600

    GPL housekeeping.

--- IDOSwitcher/Window1.xaml.cs
+++ IDOSwitcher/Window1.xaml.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿/*
+ * Switcheroo - The progressive-search task switcher for Windows.
+ * http://bitbucket.org/jasulak/switcheroo/
+ * Copyright 2009 James Sulak
+ * 
+ * Switcheroo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Switcheroo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Switcheroo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows;
