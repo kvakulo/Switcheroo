521ce5b158231040ab210237902e6faf05512a1d
Regin Larsen <kvakulo@gmail.com>
2014-02-18 20:21:11 +0100

    Add XamlHighligther that can be used to transform StringParts to XML

---
+++ Core.UnitTests/XamlHighlighterTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Switcheroo.Core.Matchers;
+
+namespace Switcheroo.Core.UnitTests
+{
+    [TestFixture]
+    public class XamlHighlighterTests
+    {
+        [Test]
+        public void DoesItWork()
+        {
+            var input = new StringPart("test > test-1", true);
+            var output = new XamlHighlighter().Highlight(new[] { input, new StringPart("test"), });
+            Assert.That(output, Is.EqualTo("<Bold>test &gt; test-1</Bold>test"));
+        }
+    }
+}
---
+++ Core/XamlHighlighter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using Switcheroo.Core.Matchers;
+
+namespace Switcheroo.Core
+{
+    public class XamlHighlighter
+    {
+        public string Highlight(IEnumerable<StringPart> stringParts)
+        {
+            if (stringParts == null) return string.Empty;
+
+            var xDocument = new XDocument(new XElement("Root"));
+            foreach (var stringPart in stringParts)
+            {
+                if (stringPart.IsMatch)
+                {
+                    xDocument.Root.Add(new XElement("Bold", stringPart.Value));
+                }
+                else
+                {
+                    xDocument.Root.Add(new XText(stringPart.Value));
+                }
+            }
+            return string.Join("", xDocument.Root.Nodes().Select(x => x.ToString()).ToArray());
+        }
+    }
+}
