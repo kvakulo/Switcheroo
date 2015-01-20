97e18ed5972d7d79b6af6288d96c705b1290ac87
HellBrick <hellbrick@gmail.com>
2015-01-20 22:46:58 +0300

    Moved AppWindowViewModel and WindowCloser to Switcheroo

--- Switcheroo/AppWindowViewModel.cs
+++ Switcheroo/AppWindowViewModel.cs
@@ -5,8 +5,9 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Switcheroo.Core;
 
-namespace Switcheroo.Core
+namespace Switcheroo
 {
 	public class AppWindowViewModel: INotifyPropertyChanged, IWindowText
 	{
--- Switcheroo/WindowCloser.cs
+++ Switcheroo/WindowCloser.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Switcheroo.Core
+namespace Switcheroo
 {
 	public class WindowCloser: IDisposable
 	{
