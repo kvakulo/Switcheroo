7c21644c67eea05536fe8ad20ec16be32fe06f34
Brad Rhodes <bradr@iqmetrix.com>
2016-12-01 13:37:22 -0600

    Added extensions for sorting the list of windows.

--- Switcheroo/Extensions.cs
+++ Switcheroo/Extensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using ManagedWinapi.Windows;
 
 namespace Switcheroo
 {
@@ -25,6 +26,42 @@ namespace Switcheroo
 				return;
 
 			pinnedList.RemoveAll(ptr => (long) ptr == (long) appWindow.HWnd);
+		}
+
+		public static List<AppWindowViewModel> Sort(this IEnumerable<AppWindowViewModel> windowList,
+			SystemWindow foregroundWindow, IEnumerable<IntPtr> pinnedList)
+		{
+			var result = new List<AppWindowViewModel>();
+
+			var windowArray = windowList as AppWindowViewModel[] ?? windowList.ToArray();
+			result.AddRange(windowArray.Where(w => !w.IsPinnedToBottom(pinnedList)));
+			result.AddRange(windowArray.Where(w => w.IsPinnedToBottom(pinnedList)));
+
+			var firstWindow = result.FirstOrDefault();
+
+			if (firstWindow != null && firstWindow.AppWindow.IsRelatedTo(foregroundWindow))
+			{
+				result.RemoveAt(0);
+				result.Add(firstWindow);
+			}
+
+			return result;
+		}
+
+		public static bool IsRelatedTo(this SystemWindow window1, SystemWindow window2)
+		{
+			return window1.HasTheSameHandleAs(window2) || window1.HasTheSameProcessIdAs(window2);
+		}
+
+		public static bool HasTheSameHandleAs(this SystemWindow window1, SystemWindow window2)
+		{
+			
+            return window1.HWnd == window2.HWnd;
+		}
+
+		public static bool HasTheSameProcessIdAs(this SystemWindow window1, SystemWindow window2)
+		{
+			return window1.Process.Id == window2.Process.Id;
 		}
 	}
 }
